ea47ce6096a4e7d6a58b2e7fa7aa60f4ec33f58f
kkathish <kkathish@kali.kathish>
2026-02-08 10:40:58 -0500

    Add automated superuser creation for Render deployment

---
+++ core/management/commands/createsuperuser_if_none_exists.py
@@ -0,0 +1,22 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Create a superuser if none exists'
+
+    def handle(self, *args, **options):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not username or not password:
+            self.stdout.write(self.style.WARNING('Superuser credentials not found in environment variables. Skipping creation.'))
+            return
+
+        if not User.objects.filter(username=username).exists():
+            self.stdout.write(f'Creating superuser for {username}...')
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Superuser {username} already exists.'))
